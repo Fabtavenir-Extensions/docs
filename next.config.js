0970d9b76bd73d1c4984f4456f793f6b25e223e4
flynncao <flynncao@yahoo.com>
2023-07-08 23:39:02 +0800

    feat: introduce nextra as site generator and mdx syntax

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+const withNextra = require('nextra')({
+  theme: 'nextra-theme-docs',
+  themeConfig: './theme.config.tsx',
+	defaultShowCopyCode: true,
+	staticImage: false
+})
+ 
+module.exports = withNextra({
+	/** Other next.js config */
+	reactStrictMode: true,
+})
---
+++ theme.config.tsx
@@ -0,0 +1,9 @@
+import { DocsThemeConfig } from 'nextra-theme-docs'
+
+const config: DocsThemeConfig = {
+	logo: <span>My Nextra Documentation</span>,
+  project: {
+    link: 'https://github.com/Fabtavenir-Extensions'
+  }
+}
+export default config
